56cb54351073d8b3acfd77a670e5fea818634f0c
Betty Hsu <997666aa@gmail.com>
2023-06-21 18:43:10 +0800

    simple tictactoe finished

--- src/components/BoardView.vue
+++ src/components/BoardView.vue
@@ -1,22 +1,79 @@
-<script>
-import { computed, ref } from 'vue'
-export default {
-  setup() { 
-    const player = ref('X')
-    const squares = ref([
-      ['', '', ''],
-      ['', '', ''],
-      ['', '', ''],
-    ])
-  }
-}
-</script>
-
 <template>
   <div class="container">
+    <h2 v-if="winner">Winner: {{ winner }}</h2>
+    <h2 v-else>Player's Move: {{ player }}</h2>
+    <button @click="reset" class="btn btn-success mb-3">Reset</button>
+    <div v-for="x in 3" :key="x" class="row">
+      <button v-for="y in 3" :key="y" class="square" @click.stop="move(x, y)">
+        {{ squares[x - 1][y - 1] }}
+      </button>
+    </div>
+
+    <h2 class="mt-5">History</h2>
+    <div v-for="(game, idx) in history" :key="idx">
+      Game {{ idx + 1 }}: {{ game }} won
+    </div>
   </div>
 </template>
-<style scoped> 
+
+<script setup>
+import { computed, onMounted, ref, watch } from "vue";
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
+const player = ref("X");
+const squares = ref([
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+]);
+const winner = computed(() => calculateWinner(squares.value.flat()));
+const move = (x, y) => {
+  if (winner.value) return;
+  squares.value[x - 1][y - 1] = player.value;
+  player.value = player.value === "O" ? "X" : "O";
+};
+const reset = () => {
+  player.value = "X";
+  squares.value = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+};
+const history = ref([]);
+
+watch(winner, (current, previous) => {
+  if (current && !previous) {
+    history.value.push(current);
+    localStorage.setItem("history", JSON.stringify(history.value));
+  }
+});
+
+onMounted(() => {
+  history.value = JSON.parse(localStorage.getItem("history")) ?? [];
+});
+</script>
+
+<style scoped>
 .square {
   background: #fff;
   border: 1px solid #999;
