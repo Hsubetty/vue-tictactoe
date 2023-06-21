3479fb2657b5c35b65c267100d8c7a30737d2f24
Betty Hsu <997666aa@gmail.com>
2023-06-21 18:01:39 +0800

    first commit

---
+++ src/components/BoardView.vue
@@ -0,0 +1,34 @@
+<script>
+import { computed, ref } from 'vue'
+export default {
+  setup() { 
+    const player = ref('X')
+    const squares = ref([
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ])
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+  </div>
+</template>
+<style scoped> 
+.square {
+  background: #fff;
+  border: 1px solid #999;
+  float: left;
+  font-size: 70px;
+  font-weight: bold;
+  line-height: 34px;
+  height: 100px;
+  margin-right: -1px;
+  margin-top: -1px;
+  padding: 0;
+  text-align: center;
+  width: 100px;
+}
+</style>
